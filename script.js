491160704f75c6291b7126c679a5e7855e8e204d
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-11-18 16:50:48 -0600

    Added search for projects

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
-const btnViewProjects = document.getElementById('btnProjects');
+const scrollController = (function() {
+   const btnViewProjects = document.getElementById('btnProjects');
+   
+   btnViewProjects.addEventListener('click', () => {
+      document.getElementById("projectsScroll").scrollIntoView({ behavior: "smooth", });
+   })
+})();
 
-btnViewProjects.addEventListener('click', () => {
-   document.getElementById("projectsScroll").scrollIntoView({ behavior: "smooth", });
-})
+
+function search() {
+   const projectCards = document.querySelectorAll('.project-card');
+   
+   const listings = [
+      {
+         title: 'calculator',
+      },
+      {
+         title: 'tic tac toe',
+      },
+      {
+         title: 'searching algorithms',
+      },
+      {
+         title: 'etch a sketch',
+      }
+   ]
+
+   const searchInput = document.getElementById('input').value.toLowerCase();
+   console.log(`${searchInput}`);
+
+   if(searchInput === "") {
+      projectCards.forEach(card => {
+         card.classList.remove('hiden');
+      });
+      return;
+   }
+
+   let searchResult = searchListings(listings, getLastIndex(), searchInput);
+   const targetDiv = document.querySelector(`[data-name='${searchResult.targetTitle}']`);
+   updateDisplayResults(targetDiv);
+
+   function searchListings(listings, lastIndex, targetWord) {
+      let looker = 0,
+          targetLocation = 0,
+          found = false;
+
+      while(looker < lastIndex && !listings[looker].title.includes(targetWord))
+         looker++;
+      targetLocation = looker;
+
+      if(listings[looker].title.includes(targetWord))
+         found = true;
+
+      let targetTitle = listings[targetLocation].title;
+
+      return { targetLocation, found, targetTitle};
+   }
+
+   function updateDisplayResults(div) {
+      projectCards.forEach(card => {
+         card.classList.add('hiden');
+      });
+   
+      div.classList.remove('hiden');
+   }
+
+   function getLastIndex() {
+      return listings.length - 1;
+   }
+}
